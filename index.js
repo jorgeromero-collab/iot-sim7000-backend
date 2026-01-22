aea93ae921cc9750d60937a21a85da2f09b9992b
jorgeromero-collab <jorge.romero@unl.edu.ec>
2026-01-22 18:11:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import admin from "firebase-admin";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Firebase
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://geolocalizacion-de-fauna-default-rtdb.firebaseio.com"
+});
+
+const db = admin.database();
+
+app.get("/data", async (req, res) => {
+  const data = {
+    bpm: Number(req.query.bpm || 0),
+    bateria: Number(req.query.bateria || 0),
+    senal: Number(req.query.senal || 0),
+    lat: Number(req.query.lat || 0),
+    lng: Number(req.query.lng || 0),
+    time: Date.now()
+  };
+
+  await db.ref("dispositivo1").set(data);
+  res.json({ status: "ok", data });
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor activo en puerto", PORT);
+});
